4655e24ef3b4c9d28863ab7549d0e7484769072a
hiNekoUwU <h1t0nek0uwu@gmail.com>
2024-04-28 16:52:21 +0300

    lab_3

--- 18.04.2024/main.cpp
+++ 18.04.2024/main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include <Windows.h>
 
 using namespace std;
 
+HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
 int main() {
-	cout << "Hello, World!" << endl << "\\\\Dennis Ritchi\\\\" << endl;
+	cout << "To be, ";
+	SetConsoleTextAttribute(hConsole, 13);
+	cout << "or not ";
+	SetConsoleTextAttribute(hConsole, 15);
+	cout << "to be: that is the question:" << endl;
+
+	cout << "Whether ";
+	SetConsoleTextAttribute(hConsole, 14);
+	cout << "'tis nobler in the mind to suffer" << endl << "The slings and arrows of outrageous fortune," << endl;
+	cout << "Or to take arms against a sea of troubles," << endl << "And by opposing end them?" << endl;
+	
+	cout << "William Shakespeare" << endl;
 }
