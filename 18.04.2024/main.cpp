d6abac5179a15ee9aa913721e14fe22f6b3aacb2
hiNekoUwU <h1t0nek0uwu@gmail.com>
2024-04-28 16:22:48 +0300

    lab_2

--- 18.04.2024/main.cpp
+++ 18.04.2024/main.cpp
@@ -1,13 +1,7 @@
 #include <iostream>
-#include <Windows.h>
 
 using namespace std;
 
-HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-
 int main() {
-	SetConsoleTextAttribute(hConsole, 14); //Changing console text color to yellow
-	cout << "\"Hello, World!\"" << endl; 
-	SetConsoleTextAttribute(hConsole, 15); //Changing console text color back to white
-	cout << "/Bjarne Stroustrup/" << endl;
+	cout << "Hello, World!" << endl << "\\\\Dennis Ritchi\\\\" << endl;
 }
