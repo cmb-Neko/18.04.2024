f73a7db6f2aa646af573f0cda724f1da32f1ab21
hiNekoUwU <h1t0nek0uwu@gmail.com>
2024-04-29 21:24:51 +0300

    hm_2

--- 18.04.2024/main.cpp
+++ 18.04.2024/main.cpp
@@ -6,11 +6,18 @@ using namespace std;
 HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
 int main() {
-	cout << "What we lost in the fire\nwe'll find in the ashes\nThere's something calling,\nsomething outside the herd\nI'm trying to wield my privilage\n";
-	cout << "in the service of a dream\nIt's not imposible to me that one\nmorning we'll see (we'll see)\n";
-	cout << "A new seed scattered on a level field" << endl;
-	cout << "I wanna watch the world burn for what it's done\nI wanna watch the world burn for what we've become\nI wanna watch the world burn for what it's done\n";
-	cout << "I wanna watch the world burn\nI wanna watch the world burn" << endl;
-
-	cout << "From \"War Music\"\nBy \"Refused\"" << endl;
+	SetConsoleTextAttribute(hConsole, 4);
+	cout << "Every ";
+	SetConsoleTextAttribute(hConsole, 12);
+	cout << "hunter ";
+	SetConsoleTextAttribute(hConsole, 14);
+	cout << "wants to ";
+	SetConsoleTextAttribute(hConsole, 10);
+	cout << "know ";
+	SetConsoleTextAttribute(hConsole, 11);
+	cout << "where ";
+	SetConsoleTextAttribute(hConsole, 9);
+	cout << "the pheasant ";
+	SetConsoleTextAttribute(hConsole, 13);
+	cout << "is" << endl;
 }
